fff15a614f5c956c1255a77a0fad6b02741517a5
kushagra-raii <kushagrarai83@gmail.com>
2023-01-21 22:13:10 +0530

    Variables And Datatypes

---
+++ datatypes.js
@@ -0,0 +1,4 @@
+"use strict";// For new version of js
+
+//alert(3+3)// not browser its node
+
---
+++ variable.js
@@ -0,0 +1,6 @@
+const clientName = "Kushagra"
+
+const height = "5.6 feet"
+let weight = "72 kg"
+
+console.table([clientName,height,weight])
