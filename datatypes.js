1e8ddef90c71fc6471fb929146109855e02f6077
kushagra-raii <kushagrarai83@gmail.com>
2023-01-21 22:38:38 +0530

    Data Types

--- datatypes.js
+++ datatypes.js
@@ -2,3 +2,14 @@
 
 //alert(3+3)// not browser its node
 
+let a =1
+console.log(typeof a)
+let b = "abc"
+console.log(typeof b)
+let c =true
+console.log(typeof c)
+let d = null
+console.log(typeof d)
+let e;
+console.log(typeof e)
+
